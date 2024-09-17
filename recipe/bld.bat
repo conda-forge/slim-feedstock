ece3e55b15a75ae7f443c6a6a3dc6acff6ed52ad
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-09-17 17:06:28 +0200

    Some help with the tooling

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,12 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
+set "HOST=x86_64-w64-mingw32"
+set "CC=%HOST%-gcc.exe"
+set "CXX=%HOST%-g++.exe"
+set "AR=%HOST%-gcc-ar.exe"
+set "RANLIB=%HOST%-gcc-ranlib.exe"
+
 cmake -G "MSYS Makefiles" ^
       %CMAKE_ARGS% ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
