891e7621c57354833d08ac6bcfe978bff2c72d4c
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-09-17 16:39:53 +0200

    Try to simplify bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,14 @@
+@echo on
+
 setlocal EnableDelayedExpansion
 
 mkdir build
 cd build
 
-:: Work around bug in mingw (we're stuck at gcc 5.3.0).
-:: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=52991
-set EXTRA_FLAGS="-mno-ms-bitfields"
-
 cmake -G "MSYS Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      %CMAKE_ARGS% ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       -DCMAKE_CXX_STANDARD_LIBRARIES:STRING="-lpsapi" ^
-      -DCMAKE_C_FLAGS="%EXTRA_FLAGS%" ^
-      -DCMAKE_CXX_FLAGS="%EXTRA_FLAGS%" ^
       ..
 if errorlevel 1 exit 1
 
